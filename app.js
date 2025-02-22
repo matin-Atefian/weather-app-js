66d903ac7cf0c9a0c5adab9009fb45a82d9c83d8
matin atefian <matinatefian186u@gmail.comm>
2025-02-22 23:46:10 +0330

    js code

--- app.js
+++ app.js
@@ -1,10 +1,50 @@
-const apiKey = "0108ac33831114c3c54918242d8b47de";
-
-async function fechdata(endpoint, city) {
-  const apiUrl = `https://api.openweathermap.org/data/2.5/${endpoint}?q=${city}&appid=${apiKey}`;
-  const fechApi = await fetch(apiUrl);
-  return fechApi.json();
+const inputCity = document.querySelector(".city"); // input
+const btn = document.querySelector(".search-btn"); // search button
+const key = "0108ac33831114c3c54918242d8b47de"; //Api key
+let name = document.querySelector(".name");
+let tempe = document.querySelector(".temp");
+let coundition = document.querySelector(".coundition");
+let Humidity = document.querySelector(".Humidity");
+let windspeed = document.querySelector(".wind");
+// serach button
+btn.addEventListener("click", () => {
+  if (inputCity.value.trim() != "") {
+    // console.log(inputCity.value);
+    weatherInfo(inputCity.value);
+    inputCity.value = "";
+    inputCity.blur();
+  }
+});
+//serach with enter
+inputCity.addEventListener("keydown", (event) => {
+  if (event.key == "Enter" && inputCity.value.trim() != "") {
+    // console.log(inputCity.value);
+    weatherInfo(inputCity.value);
+    inputCity.value = "";
+    inputCity.blur();
+  }
+  //  console.log(event);
+});
+async function fechData(endPoint, city) {
+  const url = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${key}`;
+  const response = await fetch(url);
+  return response.json();
 }
-async function updateInfo(city) {
-  const weather_Data = await fechdata("weather", city);
+
+async function weatherInfo(city) {
+  const weatherData = await fechData("weather", city);
+
+  const {
+    name: country,
+    main: { temp, humidity },
+    weather: [{ id, main }],
+    wind: { speed },
+  } = weatherData;
+  name.textContent = country;
+  tempe.textContent = Math.floor(temp - 273);
+  Humidity.textContent = humidity + "%";
+  coundition.textContent = main;
+  windspeed.textContent = Math.round(speed);
+
+  console.log(weatherData);
 }
